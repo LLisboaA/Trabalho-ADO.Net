9170e79fc204017c2089527304cfa0c8764a8700
Lucas <llisboaa@outlook.com>
2014-04-01 14:37:49 -0300

    Trabalho ADO.Net v0.1

---
+++ TrabalhoADO/Areas/Painel/Models/Empresa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace TrabalhoADO.Areas.Painel.Models
+{
+    public class Empresa
+    {
+        public int EmpresaId { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public string Endereco { get; set; }
+    }
+}
